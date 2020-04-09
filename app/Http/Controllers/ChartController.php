9ba8f7effd3a0ff5d60c1a179fb465e03998538f
Alex <alexnewzniyo@gmail.com>
2020-04-09 11:57:27 +0300

    user interface and the database

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Prediction;
 class ChartController extends Controller
 {
-    //
+    function index(){
+        $users = Prediction::select(\DB::raw("COUNT(*) as count"))
+        ->whereYear('created_at', date('Y'))
+        ->groupBy(\DB::raw("Month(created_at)"))
+        ->pluck('count');
+        return view('chart', compact('users'));
+        
+    }
 }
--- resources/views/predictions.blade.php
+++ resources/views/predictions.blade.php
@@ -14,7 +14,8 @@ Predictions
     <small id="imageHelp" class="form-text text-muted">** Strictly, Only mammogram images should be uploaded</small>
   </div>
  <center>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary btn-out-dashed">Submit</button>
+  <button type="reset" class="btn btn-danger btn-out-dashed">Cancel</button>
 </center>
 </form>
 </div>
