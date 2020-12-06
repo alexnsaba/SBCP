bddaacbc20d379890456bb69bb5a7d03400fc771
Alex <alexnewzniyo@gmail.com>
2020-12-06 09:06:32 +0300

    final SBCP documents

--- resources/views/predictions.blade.php
+++ resources/views/predictions.blade.php
@@ -16,10 +16,7 @@ Predictions
 {{--      <input type="file" name="image" class="form-control form-control-primary" id="image" accept="image/*" onchange="previewFile(this);" required>--}}
     <small id="imageHelp" class="form-text text-muted">** Strictly, Only mammogram images should be uploaded</small>
   </div>
-  <center>
-  <img id="previewImg" src="/examples/images/transparent.png" height="200" width="200">
-  </center><br/>
- <center>
+
   <button type="submit" class="btn btn-primary btn-out-dashed">Predict</button>
   <!--
   <button type="reset" class="btn btn-danger btn-out-dashed">Cancel</button>
